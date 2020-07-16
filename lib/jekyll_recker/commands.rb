d3a976ba0f6423a0f5621de7670ce4989308d4f2
Alex Recker <alex@reckerfamily.com>
2020-07-16 01:14:41 -0500

    Today command

--- lib/jekyll_recker/commands.rb
+++ lib/jekyll_recker/commands.rb
@@ -3,7 +3,7 @@
 module JekyllRecker
   # Commands
   module Commands
-    # Share
+    # Share Command
     class Share < Jekyll::Command
       include Mixins::Logging
 
@@ -21,6 +21,32 @@ module JekyllRecker
         site.reset
         site.read
         Social.action(site, args, options)
+      rescue StandardError => e
+        logger.error e.message
+        exit 1
+      end
+    end
+
+    # Today Command
+    class Today < Jekyll::Command
+      include Mixins::Logging
+
+      def self.init_with_program(prog)
+        prog.command(:today) do |c|
+          c.syntax 'today'
+          c.description 'Open today\'s entry'
+          c.option 'dry', '-d', '--dry', 'perform dry run'
+          c.action { |args, opts| action(args, opts) }
+        end
+      end
+
+      def self.action(args, options)
+        filename = Time.now.strftime("%Y-%m-%d-%Y-%m-%d.html.md")
+        logger.info "#{filename}"
+        # site = Jekyll::Site.new(configuration_from_options(options))
+        # site.reset
+        # site.read
+        # Social.action(site, args, options)
       rescue StandardError => e
         logger.error e.message
         exit 1
