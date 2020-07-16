ffe7077a9af07948c0a068e3bd3548dcc63f51a4
Alex Recker <alex@reckerfamily.com>
2020-07-16 15:39:47 -0500

    Memory generator

--- lib/jekyll_recker/generators.rb
+++ lib/jekyll_recker/generators.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'mini_magick'
 require 'fastimage'
+require 'mini_magick'
 
 module JekyllRecker
   module Generators
@@ -185,6 +185,30 @@ module JekyllRecker
           'poop',
           'shit',
         ]
+      end
+    end
+
+    # Memory Size Generator
+    class Memory < Jekyll::Generator
+      include Stats
+
+      KEY = 'memory'
+
+      def crunch
+        results = Hash.new(0)
+        entries.each do |entry|
+          results['chars'] += entry.content.size
+          results['spaces'] += entry.content.count(' ')
+          results['size'] += entry.content.bytes.to_a.length
+        end
+        results['size'] = bytes_to_megabytes(results['size'])
+        results
+      end
+
+      private
+
+      def bytes_to_megabytes(bytes)
+        (bytes / (1024.0 * 1024.0)).to_f.round(4)
       end
     end
   end
